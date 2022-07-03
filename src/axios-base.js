42856aaae76ba698d8ccc4aed091b8fc70349ed1
Tubshinbat <tuvshinbat596@gmail.com>
2022-07-03 15:09:49 +0800

    pnal

--- src/axios-base.js
+++ src/axios-base.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://beta.zaya-ananda.com/api/",
+  baseURL: "http://admin.zaya-ananda.com/api/",
 });
 
 instance.defaults.withCredentials = true;
